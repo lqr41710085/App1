ee895658a2f3adbbe0e1883a84f10c1c8f8f760b
lqr <1401219230@qq.com>
2020-09-16 20:20:01 +0800

    btn click for points:+3 +2 +1 reset

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.app1;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
@@ -21,29 +17,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn= findViewById(R.id.button);
-
-       btn.setOnClickListener(new View.OnClickListener() {
+        Button btn1= findViewById(R.id.button1);
+        Button btn2= findViewById(R.id.button2);
+        Button btn3= findViewById(R.id.button3);
+        Button reset= findViewById(R.id.reset);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView ss=findViewById(R.id.number);
-                String s=ss.getText().toString();
-                double number=Double.valueOf(s);
-                TextView result=findViewById(R.id.result);
-                double re=(number-32)*1.8;
-                result.setText("我是setonclick");
-
+                TextView grade=findViewById(R.id.grades);
+                String gra=grade.getText().toString();
+                grade.setText(String.valueOf(Integer.valueOf(gra)+3));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView grade=findViewById(R.id.grades);
+                String gra=grade.getText().toString();
+                grade.setText(String.valueOf(Integer.valueOf(gra)+2));
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView grade=findViewById(R.id.grades);
+                String gra=grade.getText().toString();
+                grade.setText(String.valueOf(Integer.valueOf(gra)+1));
+            }
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView grade=findViewById(R.id.grades);
+                String gra=grade.getText().toString();
+                grade.setText("0");
             }
         });
     }
-    public void btnclick(View view){
-        TextView ss=findViewById(R.id.number);
-        String s=ss.getText().toString();
-        double number=Double.valueOf(s);
-        TextView result=findViewById(R.id.result);
-        double re=(number-32)*1.8;
-        result.setText("我是onclick");
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
