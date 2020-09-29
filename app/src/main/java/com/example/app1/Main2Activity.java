36bb0209e849093c5d4f92cd210cf797aba2eda3
lqr <1401219230@qq.com>
2020-09-29 15:59:42 +0800

    data is reserved in rate.xml and use a thread to connect to internet,but not get the rate

--- app/src/main/java/com/example/app1/Main2Activity.java
+++ app/src/main/java/com/example/app1/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,25 +14,33 @@ import org.w3c.dom.Text;
 public class Main2Activity extends AppCompatActivity {
     TextView dol_text,eur_text,jap_text,hk_text;
     private static final String TAG="Main2Activity";
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        sp=this.getSharedPreferences("rate",MODE_PRIVATE);
         Intent v=getIntent();
+        /*
         double dol= v.getDoubleExtra("dol",0.0f);
         double eur=v.getDoubleExtra("eur",0.0f);
         double jap=v.getDoubleExtra("jap",0.0f);
         double hk=v.getDoubleExtra("hk",0.0f);
+        */
+        float dol=sp.getFloat("dol",0.0f);
+        float eur=sp.getFloat("eur",0.0f);
+        float jap=sp.getFloat("jap",0.0f);
+        float hk=sp.getFloat("hk",0.0f);
         dol_text=findViewById(R.id.dol);
         eur_text=findViewById(R.id.eur);
         jap_text=findViewById(R.id.jap);
         hk_text=findViewById(R.id.hk);
 
-        dol_text.setText(String.valueOf(dol));
-        eur_text.setText(String.valueOf(eur));
-        jap_text.setText(String.valueOf(jap));
-        hk_text.setText(String.valueOf(hk));
+        dol_text.setText(String.format("%.3f",dol).toString());
+        eur_text.setText(String.format("%.3f",eur).toString());
+        jap_text.setText(String.format("%.3f",jap).toString());
+        hk_text.setText(String.format("%.3f",hk).toString());
 
     }
     public void save(View v){
@@ -47,13 +56,21 @@ public class Main2Activity extends AppCompatActivity {
         Double jap=Double.parseDouble(jap_text.getText().toString());
         Double hk=Double.parseDouble(hk_text.getText().toString());
 
-        Bundle bdl=new Bundle();
+       sp=this.getSharedPreferences("rate",MODE_PRIVATE);
+        editor=sp.edit();
+        editor.putFloat("dol",dol.floatValue());
+        editor.putFloat("eur",eur.floatValue());
+        editor.putFloat("jap",jap.floatValue());
+        editor.putFloat("hk",hk.floatValue());
+        editor.apply();
+
+       /* Bundle bdl=new Bundle();
         bdl.putDouble("dol",dol);
         bdl.putDouble("eur",eur);
         bdl.putDouble("jap",jap);
         bdl.putDouble("hk",hk);
 
-        in.putExtras(bdl);
+        in.putExtras(bdl);*/
         setResult(4,in);
         finish();
 
--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.app1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -14,19 +17,91 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements Runnable{
     double dol,eur,jap,hk;
     TextView edit,text;
     private  static final String TAG="MainActivity";
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    Handler handler;
+    @Override
+    public void run() {
+        String html="";
+        URL url =null;
+        Log.i(TAG,"hhhrun:ok");
+        try {
+            url = new URL("https://www.usd-cny.com/bankofchina.htm");
+            HttpURLConnection http=(HttpURLConnection)url.openConnection();
+            Log.i(TAG,"hhhconnect to internet");
+            InputStream in = http.getInputStream();
+            html=inputStream2String(in);
+            Log.i(TAG,"hhhhtml:ok");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Message msg=handler.obtainMessage(4);
+        msg.obj=html;
+        handler.sendMessage(msg);
+
+        Log.i(TAG,"hhhsend:"+html);
+    }
+
+    private String inputStream2String(InputStream inputStream) throws IOException{
+        String str="";
+        BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"gb2312"));
+        StringBuffer sb=new StringBuffer();
+        while((str=reader.readLine())!=null){
+            Log.i(TAG,"in2str:"+str);
+            sb.append(str).append("\n");
+        }
+        return  sb.toString();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dol=0.147;
+        Thread t=new Thread(this);
+        t.start();
+        Log.i(TAG,"hhhstart:ok");
+        handler=new Handler(){
+            public void handleMessage(Message msg){
+                if(msg.what==4){
+                    String str=(String)msg.obj;
+                    Log.i(TAG,"hhhhandler:"+str);
+
+                }
+                super.handleMessage(msg);
+            }
+        };
+        sp=this.getSharedPreferences("rate",MODE_PRIVATE);
+        editor=sp.edit();
+        /*.putFloat("dol",0.147f);
+        editor.putFloat("eur",0.125f);
+        editor.putFloat("jap",15.372f);
+        editor.putFloat("hk",1.149f);
+        editor.apply();*/
+        dol=sp.getFloat("dol",0.0f);
+        eur=sp.getFloat("eur",0.0f);
+        jap=sp.getFloat("jap",0.0f);
+        hk=sp.getFloat("hk",0.0f);
+        /* dol=0.147;
         eur=0.125;
         jap=15.372;
         hk=1.149;
-
+        */
     }
     //在xml中btn绑定事件，四个按钮可以绑定一个事件，再依次判断
     public void btn(View btn){
@@ -65,10 +140,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void config(View v){
         Intent in = new Intent(this,Main2Activity.class);
-        in.putExtra("dol",dol);
+        /*in.putExtra("dol",dol);
         in.putExtra("eur",eur);
         in.putExtra("jap",jap);
         in.putExtra("hk",hk);
+
+        */
         Log.i(TAG,"config: ok");
         startActivityForResult(in,1);
 
@@ -77,13 +154,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode==1&&resultCode==4){
-            Bundle bdl=data.getExtras();
+          /*  Bundle bdl=data.getExtras();
             dol=bdl.getDouble("dol",1.0f);
             eur=bdl.getDouble("eur",1.0f);
             jap=bdl.getDouble("jap",1.0f);
             hk=bdl.getDouble("hk",1.0f);
-            Log.i(TAG,"dol:"+dol);
 
+*/
+            dol=sp.getFloat("dol",0.0f);
+            eur=sp.getFloat("eur",0.0f);
+            jap=sp.getFloat("jap",0.0f);
+            hk=sp.getFloat("hk",0.0f);
+          Log.i(TAG,"dol:"+dol);
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
@@ -111,4 +193,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
